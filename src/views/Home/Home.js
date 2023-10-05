fad171e0fc94e8671f843347368f20ba94ca9c63
shwetapund <pundshweta2020@gmail.com>
2023-10-04 18:41:18 -0700

    set background image

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Home.css';
 import { Link } from 'react-router-dom';
+
+
+
 export default function Home() {
     return(
         <>
+            <div className='main-title-container'>
+            <h1 className='blog-name'>New Craze</h1>
+            </div>
+
         <div>
         <h1>Home</h1>
         <Link to="/post">Post</Link>
